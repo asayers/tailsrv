1c80c82730ba00983f4a2a38d563b0e9d6840645
Alex Sayers <alex.sayers@gmail.com>
2017-11-24 01:44:59 +0900

    Move header grammar entirely into header module

--- src/header.rs
+++ src/header.rs
@@ -1,4 +1,5 @@
 use index::*;
+use nom;
 use std::path::*;
 use std::str;
 
@@ -27,3 +28,30 @@ named!(stream_header<Header>, do_parse!(
     (Header::Stream{ path: path, index: index })
 ));
 named!(path<PathBuf>, map!(take_until!(" "), |x| Path::new(str::from_utf8(x).unwrap()).to_owned()));
+
+// TODO: Unit tests
+named!(index<Index>, alt!( byte_idx | line_idx | seqnum_idx | start_idx | end_idx ));
+named!(byte_idx<Index>, do_parse!(
+    tag!("byte ") >>
+    bytes: natural >>
+    (Index::Byte(bytes as i64))
+));
+named!(line_idx<Index>, do_parse!(
+    tag!("line ") >>
+    lines: natural >>
+    (Index::Line(lines as i64))
+));
+named!(seqnum_idx<Index>, do_parse!(
+    tag!("seqnum ") >>
+    seqnum: natural >>
+    (Index::SeqNum(seqnum))
+));
+named!(start_idx<Index>, do_parse!(
+    tag!("start") >>
+    (Index::Start)
+));
+named!(end_idx<Index>, do_parse!(
+    tag!("end") >>
+    (Index::End)
+));
+named!(natural<usize>, flat_map!(recognize!(nom::digit), parse_to!(usize)));
--- src/index/mod.rs
+++ src/index/mod.rs
@@ -1,4 +1,3 @@
-use nom;
 use std::fs::File;
 use std::ops::Neg;
 use types::*;
@@ -15,35 +14,10 @@ pub enum Index {
     End,
 }
 
-// TODO: Unit tests
-named!(pub index<Index>, alt!( byte_idx | line_idx | seqnum_idx | start_idx | end_idx ));
-named!(byte_idx<Index>, do_parse!(
-    tag!("byte ") >>
-    bytes: natural >>
-    (Index::Byte(bytes as i64))
-));
-named!(line_idx<Index>, do_parse!(
-    tag!("line ") >>
-    lines: natural >>
-    (Index::Line(lines as i64))
-));
-named!(seqnum_idx<Index>, do_parse!(
-    tag!("seqnum ") >>
-    seqnum: natural >>
-    (Index::SeqNum(seqnum))
-));
-named!(start_idx<Index>, do_parse!(
-    tag!("start") >>
-    (Index::Start)
-));
-named!(end_idx<Index>, do_parse!(
-    tag!("end") >>
-    (Index::End)
-));
-named!(natural<usize>, flat_map!(recognize!(nom::digit), parse_to!(usize)));
-
-/// Resolves an index to a byte offset. `None` means that the index refers to a position beyond the
-/// end of the file and we don't have enough information to resolve it yet.
+/// Resolves an index to a byte offset.
+///
+/// `None` means that the index refers to a position beyond the end of the file and we don't have
+/// enough information to resolve it yet.
 // TODO: Unit tests
 pub fn resolve_index(file: &mut File, idx: Index) -> Result<Option<usize>> {
     Ok(match idx {
