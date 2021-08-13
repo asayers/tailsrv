b1c20ee7d4d41aa52ca6358d62b34a536cf8152a
Alex Sayers <alex@asayers.com>
2021-08-14 06:22:03 +0900

    Remove a broken doc test

--- src/index/line.rs
+++ src/index/line.rs
@@ -5,13 +5,6 @@ use std::fs::File;
 
 /// Returns the byte-offset of the start of the `cnt`th line within the given file. Line- and
 /// byte-counts are both 0-indexed.
-///
-/// ```
-/// assert_eq!(linebyte("test_data/file.txt", 0), Some(0));
-/// assert_eq!(linebyte("test_data/file.txt", 1), Some(4));
-/// assert_eq!(linebyte("test_data/file.txt", 2), Some(11));
-/// assert_eq!(linebyte("test_data/file.txt", 3), None);
-/// ```
 // FIXME: Let's not mmap log files...
 pub fn linebyte(file: &File, cnt: usize, delim: u8) -> Option<usize> {
     if cnt == 0 {
